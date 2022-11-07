1b12f42432d861e2751654fa23b64318fbf5e6ff
JoshDaBosh <jwanggt@gmail.com>
2022-11-06 21:08:04 -0500

    bit more responsive

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -9,15 +9,15 @@ export default function Home() {
   return (
     <Center h="100vh" w="vw" style={{overflow:"hidden"}}>
       <Box w="75%">
-        <Heading fontSize="9xl" marginLeft="-0.02em">
+        <Heading fontSize={["7xl", "7xl", "9xl"]} marginLeft="-0.02em">
           ClubCall
         </Heading>
-        <Heading fontSize="3xl" color="gray" marginTop="-0.8em">
+        <Heading fontSize={["2xl", "2xl", "3xl"]} color="gray" marginTop="-0.8em">
           Aggregate Discord announcements. Anywhere.
         </Heading>
 
         <Link to="/dashboard">
-          <Button colorScheme="purple" size="lg" marginTop="1em">
+          <Button colorScheme="purple" size={["md", "md", "lg"]} marginTop="1em">
             Check it out <ArrowForwardIcon />
           </Button>
         </Link>
